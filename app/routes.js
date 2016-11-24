8cb8df07a39dbca4f0551c4723b50eca72251272
flodi <florent@flodi.fr>
2016-11-24 14:45:49 +0100

    Hard work

--- app/routes.js
+++ app/routes.js
@@ -9,7 +9,7 @@
      * All Routing here...
      * Each Route can resolve some datas before send in controller
      */
-    angular.module('app', ['ngRoute', 'ngAnimate']).config(function($routeProvider) {
+    angular.module('app', ['ngRoute' , 'ngMaterial']).config(function($routeProvider) {
         $routeProvider
             .when('/', {
                 templateUrl: 'app/main/main.html',
